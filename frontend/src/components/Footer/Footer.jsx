159f4ac745f078c0883a79726535cba1e3722a7c
Janardhan-2003 <kokatam71714@gmail.com>
2025-08-13 11:30:44 +0530

    changed title

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,8 +5,6 @@ const Footer = () => {
     <div>
         <footer className='bg-transparent border-t-1 border-slate-700 text-white text-center py-4 bottom-0 w-full flex justify-center gap-6 sticky'>
            <p>&copy; NimbuS. All rights reserved.</p>
-            <p>Developed by Your Name</p>
-            <p>Contact: <a href="mailto:"></a> </p>
         </footer>
     </div>
   )

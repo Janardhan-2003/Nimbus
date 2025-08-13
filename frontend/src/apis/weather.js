cbb50d4f337ee716bcfe234dee3783d25e8c0ff8
Janardhan-2003 <kokatam71714@gmail.com>
2025-08-13 11:07:02 +0530

    Small changes in backend

--- frontend/src/apis/weather.js
+++ frontend/src/apis/weather.js
@@ -1,7 +1,7 @@
 
 export async function Weather(city) {
     try{
-    const response=await fetch(`http://localhost:5000/weather?city=${city}`)
+    const response=await fetch(`https://nimbus-m4jq.onrender.com/weather?city=${city}`)
 
     if (!response.ok) {
         throw new Error('Network response was not ok');

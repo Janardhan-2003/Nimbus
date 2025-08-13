14cdcee068254e5db8027ee7080cfc5bbea53b13
Janardhan-2003 <kokatam71714@gmail.com>
2025-08-13 11:13:03 +0530

    Small changes in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SpeedInsights>
     <App />
+    </SpeedInsights>
   </StrictMode>,
 )
